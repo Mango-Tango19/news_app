fcd99c90707973011f891223667c3f4b698c55c1
Mango-Tango19 <thisisdasha@gmail.com>
2022-02-25 14:26:10 +0300

    handle errors

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,34 @@ import { todoItem } from "./model";
 import TodoList from "./components/todoList/todoList";
 
 const App: React.FC = () => {
-
-  const [todo, setTodo] = useState<string>('')
-  const [todos, setTodos] = useState<todoItem[]>([])
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<todoItem[]>([]);
+  const [emptyInput, setEmptyInput] = useState<boolean>(false);
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    setTodos([...todos, {id: Date.now(), todo, isDone: false}])
-    setTodo('')
-  }
+    setEmptyInput(false);
+    e.preventDefault();
+    if (!todo) {
+      setEmptyInput(true);
+      return;
+    }
+    setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+    setTodo("");
+  };
 
-
-
+  const errorInputElement = (
+    <div className='alert alert-primary mt-4' role='alert'>
+      Please, type smth
+    </div>
+  );
 
   return (
     <div className='container'>
       <div className='row justify-content-md-center'>
         <div className='col col-lg-2'>Tasks</div>
         <InputField todo={todo} setTodo={setTodo} handleSubmit={handleSubmit} />
-        <TodoList todos={todos} setTodos={setTodos}/>
+        {emptyInput ? errorInputElement : null}
+        <TodoList todos={todos} setTodos={setTodos} />
       </div>
     </div>
   );
